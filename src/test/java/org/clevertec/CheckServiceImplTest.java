320c2bec163bad5d291e30bd7f739ad998ff6940
MaxFedosenko <kpabomakc@yandex.ru>
2022-12-21 23:43:59 +0200

    changed of .gitignore

--- src/test/java/org/clevertec/CheckServiceImplTest.java
+++ src/test/java/org/clevertec/CheckServiceImplTest.java
@@ -1,18 +1,22 @@
 package org.clevertec;
 
+import org.clevertec.withJavaCore.services.CheckService;
+import org.clevertec.withJavaCore.services.Validator;
 import org.clevertec.withJavaCore.services.impl.CheckServiceImpl;
+import org.clevertec.withJavaCore.services.impl.ValidatorImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CheckServiceImplTest {
     
-    private CheckServiceImpl service = new CheckServiceImpl();
+    private CheckService service = new CheckServiceImpl();
+    private Validator validator = new ValidatorImpl();
     
     @Test
     public void testDiscountOfQuantity () {
         int v1 = 5;
         double expected = 0.1;
-        double actual = service.discountOfQuantity(v1);
+        double actual = validator.validateDiscountOfQuantity(v1);
         Assertions.assertEquals(expected, actual);
     }
     
